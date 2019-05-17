72c1586628b57e487a40e00ec796a2cc30a9d714
daigo-fujioka <work.daigo@gmail.com>
2019-05-17 19:17:52 +0900

    Summary(reqired)

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,8 @@
 require_relative 'boot'
-
 require 'rails/all'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module ChatSpace
   class Application < Rails::Application
     config.generators do |g|
@@ -14,5 +11,6 @@ module ChatSpace
       g.helper false
       g.test_framework false
     end
+    config.i18n.default_locale = :ja
   end
 end
