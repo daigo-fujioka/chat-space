4f568d7a32c3298dc38a5ffdcd419d6e03940f95
daigo-fujioka <work.daigo@gmail.com>
2019-05-26 14:56:14 +0900

    Summry(required)

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -24,7 +24,6 @@ $(function(){
        })
 
        .done(function(users){
-         $("#user-search-result").empty();
          if (users.length !== 0){
              users.forEach(function(user){
               appendUser(user);
