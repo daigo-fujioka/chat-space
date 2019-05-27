f6f5dcb5ea2245e563f8cc26a6eea38dec881402
daigo-fujioka <work.daigo@gmail.com>
2019-05-27 09:58:25 +0900

    Summry(required)

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -15,7 +15,6 @@ $(function(){
     }
     $("#user-search-field").on("keyup", function(){
        var input = $("#user-search-field").val();
-
        $.ajax({
            type: 'GET',
            url: '/users/search',
@@ -24,6 +23,7 @@ $(function(){
        })
 
        .done(function(users){
+        $("#user-search-result").empty();
          if (users.length !== 0){
              users.forEach(function(user){
               appendUser(user);
@@ -42,22 +42,24 @@ $(function(){
         
     });
     var member = $('#chat-group-users')
-    function addUser(user_name){
-        var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-        <input name='group[user_ids][]' type='hidden' value=''>
+    function addUser(user_name,user_id){
+        var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-'>
+        <input name='group[user_ids][]' type='hidden' value='${user_id}'>
         <p class='chat-group-user__name'>${user_name}</p>
         <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
       </div>`
       member.append(html)
     }
     $(document).on("click", ".user-search-add", function(){
-        var name = $(this).attr('data-user-name')
-        addUser(name);
+        var name = $(this).attr("data-user-name");
+        var id = $(this).attr("data-user-id");
+        addUser(name,id);
         $(this).parent().remove();
     });
     $(document).on("click",".user-search-remove", function(){
         $(this).parent().remove();
     })
+
     
 });
 
