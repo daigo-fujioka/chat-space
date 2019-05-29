894aebd28679235008b914d1c818def9b84c0e87
daigo-fujioka <work.daigo@gmail.com>
2019-05-29 19:09:41 +0900

    summry(requierd)

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :ssh_options, auth_methods: ['publickey'],
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
-
+set :bundle_gemfile,  "server/Gemfile"
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
 task :restart do
